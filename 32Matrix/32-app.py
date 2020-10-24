40fdb611c848730155e319f30461aa3932d09f7b
Benny Mirisola <bennymirisola@gmail.com>
2020-10-24 08:31:43 +0100

    changed scroll repeats to infinite

--- 32Matrix/32-app.py
+++ 32Matrix/32-app.py
@@ -13,32 +13,35 @@ pixel_pin = board.D21
 num_pixels = 256
 display_width = 32
 display_height = 8
-matrixbrightness = 0.1
-scrollSpeed = 0.06 #adjust the scrolling speed here, smaller number=faster scroll
-TextColor = (255,255,255) #set the color of your text here in RGB, default is white
-repeats = 2 #number of times the text will scroll across the matrix
+matrixbrightness = .01
+scrollSpeed = 0.06  # adjust the scrolling speed here, smaller number=faster scroll
+TextColor = (255, 255, 255)  # set the color of your text here in RGB, default is white
+repeats = 2  # number of times the text will scroll across the matrix
 
 ORDER = neopixel.GRB
 pixels = neopixel.NeoPixel(pixel_pin, num_pixels, brightness=matrixbrightness, auto_write=False, pixel_order=ORDER)
 rotation = 0
 
-#load your font
-#font = ImageFont.truetype("LiberationMono-Regular.ttf", 8)
-#5x7.ttf font is easier to read and available for download for personal use from the Internet
+# load your font
+# font = ImageFont.truetype("LiberationMono-Regular.ttf", 8)
+# 5x7.ttf font is easier to read and available for download for personal use from the Internet
 font = ImageFont.truetype("5x7.ttf", 8)
 
-#for the Adafruit NeoMatrix grid
-def getIndex(x, y):        
-    x = display_width-x-1    
-    return (x*8)+y
 
-#use for the flex grid
+# for the Adafruit NeoMatrix grid
+def getIndex(x, y):
+    x = display_width - x - 1
+    return (x * 8) + y
+
+
+# use for the flex grid
 def getIndex2(x, y):
-    x = display_width-x-1
+    x = display_width - x - 1
     if x % 2 != 0:
-        return (x*8)+y
+        return (x * 8) + y
     else:
-        return (x*8)+(7-y)
+        return (x * 8) + (7 - y)
+
 
 if len(sys.argv) > 1:
     try:
@@ -47,41 +50,41 @@ if len(sys.argv) > 1:
         print("Usage: {} <rotation>".format(sys.argv[0]))
         sys.exit(1)
 
+
 def scrollText(text):
-# Measure the size of our text, we only really care about the width for the moment
-# but we could do line-by-line scroll if we used the height
+    # Measure the size of our text, we only really care about the width for the moment
+    # but we could do line-by-line scroll if we used the height
     text_width, text_height = font.getsize(text)
 
-# Create a new PIL image big enough to fit the text
+    # Create a new PIL image big enough to fit the text
     image = Image.new('P', (text_width + display_width + display_width, display_height), 0)
     draw = ImageDraw.Draw(image)
-# clear grid
-    pixels.fill((0,0,0))
+    # clear grid
+    pixels.fill((0, 0, 0))
     pixels.show()
     time.sleep(.001)
-# Draw the text into the image
+    # Draw the text into the image
     draw.text((display_width, -1), text, font=font, fill=255)
     image.save("img.png", "PNG")
     offset_x = 0
-    for i in range(repeats):
-        while True:        
-            for x in range(display_width):
-                for y in range(display_height):                    
-                    if image.getpixel((x + offset_x, y)) == 255:
-                        pixels[getIndex2(x,y)] = TextColor                        
-                    else:
-                        pixels[getIndex2(x,y)] = (0, 0, 0)                               
+    while True:
+        while True:
+          for x in range(display_width):
+              for y in range(display_height):
+                  if image.getpixel((x + offset_x, y)) == 255:
+                      pixels[getIndex2(x, y)] = TextColor
+                  else:
+                      pixels[getIndex2(x, y)] = (0, 0, 0)
+          offset_x += 1
+          if offset_x + display_width > image.size[0]:
+              offset_x = 0
+              break
 
-            offset_x += 1
-            if offset_x + display_width > image.size[0]:
-                offset_x = 0
-                break
+          pixels.show()
+          time.sleep(scrollSpeed)  # scrolling text speed
 
-            pixels.show()
-            time.sleep(scrollSpeed) #scrolling text speed
-        
-    
-@app.route('/', methods=['POST','GET'])
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         text_content = request.form['content']
@@ -90,7 +93,8 @@ def index():
         return render_template('index.html')
     else:
         return render_template('index.html')
-    
+
+
 if __name__ == '__main__':
     app.debug = True
     app.run(host='0.0.0.0')
